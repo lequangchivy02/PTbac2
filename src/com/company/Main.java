b7d8abce1cc17d2f3a06437d8d935acf070a77a4
vyoivy <lequangchivy1902@gmail.com>
2022-02-25 09:20:16 +0700

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập a và b");
         int a = sc.nextInt(), b = sc.nextInt(), hs;
         int kq= a-b;
-        System.out.println(a+"-"+b+"Là gì?");
-        System.out.println("Nhập kết quả");
+        System.out.println(a+"-"+b+" Là gì?");
+        System.out.print("Nhập kết quả: ");
         hs = sc.nextInt();
         if(kq!=hs){
             System.out.println("Kết quả sai");
